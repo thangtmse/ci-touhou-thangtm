d528b542eb8891b8b5912e708c68b530082ab171
thangtmse <thangtmse04173@fpt.edu.vn>
2017-10-12 19:51:12 +0700

    add canvas

---
+++ src/GameCanvas.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class GameCanvas extends JFrame {
+    public GameCanvas(){
+
+    }
+}
---
+++ src/GameWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    GameCanvas canvas;
+    public GameWindow(){
+        this.setSize(800,600);
+        this.setVisible(true);
+
+        this.canvas=new GameCanvas();
+        this.setContentPane(this.canvas);
+
+    }
+}
