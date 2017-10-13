9a23f160f8a2164bfe6489495c0197015edd9186
thangtmse <thangtmse04173@fpt.edu.vn>
2017-10-13 19:32:53 +0700

    addedplayer

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -1,7 +1,28 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class GameCanvas extends JFrame {
-    public GameCanvas(){
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+public class GameCanvas extends JPanel {
+    BufferedImage background;
+    BufferedImage player;
+    public GameCanvas(){
+        //1.load background
+        try {
+            background = ImageIO.read(new File("assets/images/background/0.png"));
+            player =ImageIO.read(new File("assets/images/players/straight/0.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+       g.drawImage(background,0,0,null);
+       g.drawImage(player,150,300,null);
     }
 }
--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GameWindow extends JFrame {
     GameCanvas canvas;
-    public GameWindow(){
-        this.setSize(800,600);
+
+    public GameWindow() {
+        this.setSize(800, 600);
+        this.canvas = new GameCanvas();
+        this.setContentPane(this.canvas);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setResizable(false);
+        this.canvas.setVisible(true);
         this.setVisible(true);
 
-        this.canvas=new GameCanvas();
-        this.setContentPane(this.canvas);
 
     }
+
+
 }
